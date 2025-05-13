9e3f29bd03e6bff447340548eba1e7d1d0a51d38
Simon Mueller <simon.mueller@a-sit.at>
2025-05-13 14:23:28 +0200

    Change to LocalDate

--- ehic/src/commonTest/kotlin/at/asitplus/wallet/ehic/SdJwtSerializationTest.kt
+++ ehic/src/commonTest/kotlin/at/asitplus/wallet/ehic/SdJwtSerializationTest.kt
@@ -4,6 +4,8 @@ import at.asitplus.wallet.lib.data.vckJsonSerializer
 import at.asitplus.wallet.lib.iso.vckCborSerializer
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
@@ -18,8 +20,8 @@ class SdJwtSerializationTest : FunSpec({
             socialSecurityNumber = randomString(),
             issuingAuthority = IssuingAuthority(randomString(), randomString()),
             documentNumber = randomString(),
-            issuanceDate = randomInstant(),
-            expiryDate = randomInstant(),
+            issuanceDate = randomInstant().toLocalDateTime(TimeZone.UTC).date,
+            expiryDate = randomInstant().toLocalDateTime(TimeZone.UTC).date,
         )
         val json = vckJsonSerializer.encodeToString(credential)
         vckJsonSerializer.decodeFromString<EuropeanHealthInsuranceCard>(json) shouldBe credential
