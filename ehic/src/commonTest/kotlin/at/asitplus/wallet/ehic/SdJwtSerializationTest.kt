0efccfe480d0520e02516a5cef7f64696a7daaf6
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-07-31 13:22:06 +0200

    Release 1.2.0

--- ehic/src/commonTest/kotlin/at/asitplus/wallet/ehic/SdJwtSerializationTest.kt
+++ ehic/src/commonTest/kotlin/at/asitplus/wallet/ehic/SdJwtSerializationTest.kt
@@ -1,7 +1,7 @@
 package at.asitplus.wallet.ehic
 
+import at.asitplus.signum.indispensable.cosef.io.coseCompliantSerializer
 import at.asitplus.wallet.lib.data.vckJsonSerializer
-import at.asitplus.wallet.lib.iso.vckCborSerializer
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.provided.randomLocalDate
@@ -9,7 +9,6 @@ import io.kotest.provided.randomString
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
-import kotlinx.serialization.encodeToString
 
 @OptIn(ExperimentalSerializationApi::class)
 class SdJwtSerializationTest : FunSpec({
@@ -33,8 +32,8 @@ class SdJwtSerializationTest : FunSpec({
             vckJsonSerializer.encodeToString<EuropeanHealthInsuranceCard>(credential)
         ) shouldBe credential
 
-        vckCborSerializer.decodeFromByteArray<EuropeanHealthInsuranceCard>(
-            vckCborSerializer.encodeToByteArray<EuropeanHealthInsuranceCard>(credential)
+        coseCompliantSerializer.decodeFromByteArray<EuropeanHealthInsuranceCard>(
+            coseCompliantSerializer.encodeToByteArray<EuropeanHealthInsuranceCard>(credential)
         ) shouldBe credential
     }
 })
